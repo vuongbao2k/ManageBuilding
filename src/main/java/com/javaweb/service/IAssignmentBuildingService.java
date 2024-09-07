d8e635d4b0e9bcba13779c779f01f80757901fbc
giabao <giabao@MacBook-Pro-cua-giabao.local>
2024-09-07 16:10:49 +0700

    Using cascade, many to many

--- src/main/java/com/javaweb/service/IAssignmentBuildingService.java
+++
@@ -1,8 +0,0 @@
-package com.javaweb.service;
-
-import com.javaweb.model.dto.AssignmentBuildingDTO;
-
-public interface IAssignmentBuildingService {
-    public void deleteByBuildings(Long[] ids);
-    public void addAssignmentBuildingEntity(AssignmentBuildingDTO assignmentBuildingDTO);
-}
